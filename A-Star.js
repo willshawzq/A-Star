1d9c0394357903860047b96a8e17b06aeacadfe7
willshawzq <810795297@qq.com>
2015-12-18 07:08:01 +0800

    update

--- A-Star.js
+++ A-Star.js
@@ -2,6 +2,7 @@ class AStar extends React.Component {
 	constructor(props) {
 	    super(props);
 	    this.state = {
+	    	gridSize: 20,
 			current: 84,
 			start: 84,
 			target: 215,
@@ -11,45 +12,17 @@ class AStar extends React.Component {
 			openObj: {}
 	    };
 	}
-	getMap() {
-		let map = [
-			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,3,3,3,3,3,3,3,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,1,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,2,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,3,3,3,3,3,3,3,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-			0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-		];
-		return map;
-	}
 	renderMap() {
-		let mapArr = this.getMap(),
+		let mapArr = this.props.map,
 			path = '',
 			{openArr, openObj, result} = this.state,
 			colorArr = ['white', 'green', 'red', 'gray'];
 		return mapArr.map((val, i)=>{
 			if(val === 3) this.state.closeArr.push(i);
-			if(result.indexOf(i) > -1) {
-				path = 'yellow';
-			}else {
-				path = '';
-			}
+			path = result.indexOf(i) > -1 ? 'black' : '';
 			return (
-				<span key={i} data-x={i % 20} data-y={Math.floor(i/20)} className={`star ${colorArr[val]} ${path}`}>
+				<span key={i} data-x={i % 20} data-y={Math.floor(i/20)} 
+					className={`star ${colorArr[val]} ${path}`}>
 					{openArr.indexOf(i) > -1 ? i : ''}
 				</span>
 			);
@@ -57,75 +30,84 @@ class AStar extends React.Component {
 	}
 	openFn() {
 		let current = this.state.openArr.shift();
+		/*
+		*如果此节点为目标节点，closeArr最后
+		*一次push的节点便为此节点的父节点
+		*/
 		if(current === this.state.target) {
-			//console.info(current, this.state.target);
 			this.showLine();
 			return;
 		};
-		console.info(current);
-
+		/*此节点已经走过，则放入到close数组中去*/
 		this.state.closeArr.push(current);
+		/*获取当前节点周围8个方向的兄弟节点*/
 		this.getSiblings(current);
+		/*对openArr进行排序，*/
 		let openObj = this.state.openObj;
 		this.state.openArr.sort((s, t)=>{
 			return openObj[s]['num'] - openObj[t]['num'];
 		});
-		//console.log(openObj);
-		//console.log(this.state.openArr);
+		/*递归调用*/
 		this.openFn();
 	}
 	showLine() {
-		let closeArr = this.state.closeArr;
-		let last = closeArr.pop();
+		let last = this.state.closeArr.pop();
 		this.findParent(last);
 	}
 	findParent(index) {
+		/*将查找到的路径上的元素，插入到数组头部*/
 		this.state.result.unshift(index);
+		/*获取当前值得父元素*/
 		let parent = this.state.openObj[index]['parent'];
+		/*找到初始节点时，停止回溯*/
 		if(parent == this.state.start){
+			/*重新绘制页面*/
 			this.setState({
 				result: this.state.result
 			});
-			console.log(this.state.result);
 			return;
 		}
 		this.findParent(parent);
 	}
+	/*节点估价函数*/
 	f(index) {
 		return this.g(index) + this.h(index);
 	}
+	/*计算起点到当前节点的代价（直线距离）*/
 	g(index) {
-		let start 	= [ 84 % 20, Math.floor(84 / 20)],
+		let start 	= [ this.state.start % 20, Math.floor(this.state.start / 20)],
 			current = [ index % 20, Math.floor(index / 20)];
 		let x = current[0] - start[0],
 			y = current[1] - start[1];
 		return Math.sqrt(x*x + y*y);
 	}
+	/*计算当前节点到目标节点的代价（直线距离）*/
 	h(index) {
-		let end 	= [ 215 % 20, Math.floor(215 / 20)],
+		let end 	= [ this.state.target % 20, Math.floor(this.state.target / 20)],
 			current = [ index % 20, Math.floor(index / 20)];
 		let x = end[0] - current[0],
 			y = end[1] - current[1];
 		return Math.sqrt(x*x + y*y);
 	}
 	getSiblings(index) {
-		let siblings = [
-			index - 21,
-			index - 20,
-			index - 19,
-			index - 1,
-			index + 1,
-			index + 19,
-			index + 20,
-			index + 21
-		].map((val)=>{
-			if(0 < val < 400) return val;
-		});
+		let {cols, map} = this.props,
+			len = map.length,
+			siblings = [
+				index - (cols + 1),
+				index - cols,
+				index - (cols - 1),
+				index - 1,
+				index + 1,
+				index + (cols - 1),
+				index + cols,
+				index + (cols + 1)
+			].map((val)=>{
+				if(0 < val < len) return val;
+			});
 		siblings.map((val, i)=>{
 			let included = this.state.closeArr.indexOf(val) < 0
 							&& this.state.openArr.indexOf(val) < 0;
 			if(included) {
-				//if(val == 215) console.log(index);
 				this.state.openArr.push(val);
 				this.state.openObj[val] = {
 					num: this.f(val),
@@ -134,22 +116,51 @@ class AStar extends React.Component {
 			}
 		});
 	}
-	componentDidMount() {
+	findWay() {
 		this.openFn();
 		this.setState({
 			openArr: this.state.openArr
 		});
 	}
+	resetMap() {
+
+	}
 	render() {
-		let width = Math.sqrt(this.getMap().length) * 20;
+		let {map, cols} = this.props,
+			len = map.length;
+		let width = Math.sqrt(len) * this.state.gridSize;
 		return (
 			<div className='AStar' style={{width: `${width}px`}}>
+				<button onClick={this.findWay.bind(this)}>寻路</button>
+				<button onClick={this.resetMap.bind(this)}>重置</button>
 				{this.renderMap()}
 			</div>
 		);
 	}
 }
+let map = [
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,3,3,3,3,3,3,3,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,1,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,2,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,3,3,3,3,3,3,3,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+];
 ReactDOM.render(
-	<AStar />,
+	<AStar map={map} cols={20}/>,
 	document.getElementById('example')
 );
